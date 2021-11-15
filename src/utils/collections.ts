dd76f1eed0f77f6ba100c36d51ed69d30ef11bae
Scott Hyndman <scott@madewithfelt.com>
2021-11-15 16:04:23 -0500

    Add a couple iterable generation utilities
    generate: creates a fixed-length iterable by calling a generator
    function for each element
    
    generateArray: creates an Array from the output of generate

--- src/utils/collections.ts
+++ src/utils/collections.ts
@@ -1,3 +1,13 @@
+export function* generate<T>(length: number, generator: (index: number) => T): Generator<T> {
+  for (let i = 0; i < length; i++) {
+    yield generator(i);
+  }
+}
+
+export function generateArray<T>(length: number, generator: (index: number) => T): Array<T> {
+  return Array.from(generate(length, generator));
+}
+
 export function first<T>(collection: ArrayLike<T>): T | undefined {
   return collection.length ? collection[0] : undefined;
 }
