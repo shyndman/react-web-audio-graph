ca96d25225dd55f524e992c412b720f131512580
Scott Hyndman <scott@madewithfelt.com>
2021-11-19 12:03:02 -0500

    Add a utility for zipping arrays

--- src/utils/collections.ts
+++ src/utils/collections.ts
@@ -15,3 +15,14 @@ export function first<T>(collection: ArrayLike<T>): T | undefined {
 export function last<T>(collection: ArrayLike<T>): T | undefined {
   return collection.length ? collection[collection.length - 1] : undefined;
 }
+
+export function* generateZip<A, B>(a: A[], b: B[]): Generator<[A, B]> {
+  const length = Math.min(a.length, b.length);
+  for (let index = 0; index < length; index++) {
+    yield [a[index], b[index]];
+  }
+}
+
+export function zip<A, B>(a: A[], b: B[]): [A, B][] {
+  return Array.from(generateZip(a, b));
+}
